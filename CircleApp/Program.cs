684cc094de3702fe7cae98358fc216a1e96fefa3
floatingFaez <floatingfaez@live.com>
2014-09-27 14:43:23 +0600

    Circle Appliaction Initially Uploaded

---
+++ CircleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle aCircle = new Circle();
+            Console.Write(@"Enter a Radious : ");
+            aCircle.radious = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nYour Diameter is : " + aCircle.GetDiameter());
+            Console.WriteLine(@"Your Perimeter is : " + aCircle.GetPerimeter());
+            Console.WriteLine(@"Your Area is : " + aCircle.GetArea());
+
+            Console.ReadKey();
+        }
+    }
+}
